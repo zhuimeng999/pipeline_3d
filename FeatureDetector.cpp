d973022a09b1bc7d999917fa9293f8e7a53d860f
lucius <luciusjie@gmail.com>
2020-03-23 19:41:03 +0800

    export keypoint list file

--- FeatureDetector.cpp
+++ FeatureDetector.cpp
@@ -29,10 +29,10 @@ static auto _t1 = std::chrono::high_resolution_clock::now();
 int FeatureDetector::run(const int argc, const char *argv[]) {
   boost::program_options::options_description od;
   od.add_options()("image_directory", "image directory")
-  ("output_filename", "output filename");
+  ("work_directory", "work directory");
   boost::program_options::positional_options_description pod;
   pod.add("image_directory", 1);
-  pod.add("output_filename", 1);
+  pod.add("work_directory", 1);
 
   boost::program_options::variables_map vm;
   boost::program_options::store(boost::program_options::command_line_parser(argc, argv).options(od).positional(pod)
@@ -40,23 +40,32 @@ int FeatureDetector::run(const int argc, const char *argv[]) {
   boost::program_options::notify(vm);
 
 
-  if ((!vm.count("image_directory")) or (!vm.count("output_filename"))) {
+  if ((!vm.count("image_directory")) or (!vm.count("work_directory"))) {
     std::cout << "you must privide a image directory and a output_filename" << std::endl;
     return EXIT_FAILURE;
   }
 
-  boost::filesystem::path p(vm["image_directory"].as<std::string>());
-  if (!exists(p)) {
-    std::cout << "the image directory " << p.string() << " does not exist" << std::endl;
+  boost::filesystem::path imgDir(vm["image_directory"].as<std::string>());
+  if (!exists(imgDir)) {
+    std::cout << "the image directory " << imgDir.string() << " does not exist" << std::endl;
     return EXIT_FAILURE;
-  } else if (!is_directory(p)) {
-    std::cout << p.string() << " is not a directory" << std::endl;
+  } else if (!is_directory(imgDir)) {
+    std::cout << imgDir.string() << " is not a directory" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  boost::filesystem::path workDir(vm["work_directory"].as<std::string>());
+  if (!exists(workDir)) {
+    std::cout << "the work directory " << workDir.string() << " does not exist" << std::endl;
+    return EXIT_FAILURE;
+  } else if (!is_directory(workDir)) {
+    std::cout << workDir.string() << " is not a directory" << std::endl;
     return EXIT_FAILURE;
   }
 
   std::vector<boost::filesystem::path> images;
 
-  for (auto &&x : boost::filesystem::directory_iterator(p)) {
+  for (auto &&x : boost::filesystem::directory_iterator(imgDir)) {
     if (imageTypeList.count(x.path().extension().string())) {
       images.push_back(x.path());
     }
@@ -72,19 +81,43 @@ int FeatureDetector::run(const int argc, const char *argv[]) {
   std::cout << "feature detect progress";
   boost::progress_display show_progress(images.size());
   TIMER_START();
-#pragma omp parallel default(none) shared(minHessian, show_progress, images, keypointsList, descriptorsList)
+#pragma omp parallel default(none) shared(workDir, minHessian, show_progress, images, keypointsList, descriptorsList)
   {
     cv::Ptr<cv::xfeatures2d::SIFT> detector = cv::xfeatures2d::SIFT::create(minHessian);
 #pragma omp for
     for (int i = 0; i < images.size(); i++) {
       cv::Mat img = cv::imread(images[i].string(), cv::IMREAD_UNCHANGED);
-      detector->detectAndCompute(img, cv::noArray(), keypointsList[i], descriptorsList[i]);
+      auto &kp = keypointsList[i];
+      auto &desc =  descriptorsList[i];
+      detector->detectAndCompute(img, cv::noArray(), kp, desc);
+
+      boost::filesystem::ofstream keyfile((workDir/images[i].stem()).string() + ".key");
+      keyfile << desc.rows << " " << desc.cols << std::endl;
+      for(int j = 0; j < desc.rows; j++){
+        float angleRadian = kp[j].angle*M_PI/180;
+        if(angleRadian > M_PI){
+          angleRadian = angleRadian - (M_PI*2);
+        }
+        keyfile << kp[j].pt.x << " " << kp[j].pt.y << " " << kp[j].response << " " << angleRadian << std::endl;
+        for(int k = 0; k < 6; k++){
+          for(int m = 0; m < 20; m++){
+            keyfile << " " << static_cast<unsigned int>(desc.at<unsigned char>(j, 20*k + m));
+          }
+          keyfile << std::endl;
+        }
+        for(int k = 0; k < 8; k++){
+          keyfile << " " << static_cast<unsigned int>(desc.at<unsigned char>(j, 120+k));
+        }
+        keyfile << std::endl;
+      }
+      keyfile.flush();
+      keyfile.close();
 #pragma omp critical
       ++show_progress;
     }
   }
   TIMER_END();
-
+  std::cout <<descriptorsList[0].row(0).size;
   std::vector<std::pair<int, int>> ompKeys;
   ompKeys.reserve(images.size() * (images.size() - 1) / 2);
   for (int i = 0; i < images.size(); i++) {
@@ -117,7 +150,7 @@ int FeatureDetector::run(const int argc, const char *argv[]) {
   }
   TIMER_END();
 
-  boost::filesystem::ofstream of(vm["output_filename"].as<std::string>());
+  boost::filesystem::ofstream of(workDir/boost::filesystem::path("matches.txt"));
   for(int i = 0; i < ompKeys.size(); i++){
     const auto & good_matches = allMatches[i];
     if (good_matches.size() >= 16) {
