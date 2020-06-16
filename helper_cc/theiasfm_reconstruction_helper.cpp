1d0aec16e738081281670ee346d0b0659ea559a8
lucius <luciusjie@gmail.com>
2020-06-16 15:13:46 +0800

    add demo code for cc backend

---
+++ helper_cc/theiasfm_reconstruction_helper.cpp
@@ -0,0 +1,23 @@
+#include <boost/python.hpp>
+using namespace boost::python;
+
+struct World
+{
+    World(std::string msg): msg(msg) {} // added constructor
+    void set(std::string msg) { this->msg = msg; }
+    std::string greet() { return msg; }
+    std::string msg;
+    std::string const name;
+    float value;
+};
+
+
+BOOST_PYTHON_MODULE(hello)
+{
+    class_<World>("World", init<std::string>())
+        .def("greet", &World::greet)
+        .def("set", &World::set)
+        .def_readonly("name", &World::name)
+      .def_readwrite("value", &World::value);
+    ;
+}
